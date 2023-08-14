5056272274c14a7e098fcbd60e89b8f191d0ef52
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-14 23:43:32 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+player.on('timeupdate',throttle(timeUpdate, 1000));
+
+function timeUpdate(data) {
+    const currentTime = data.seconds;
+
+    localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
