743b49800076f8752f13d4749daac4bcc29c5c81
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-14 23:43:29 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+
+function createMarkup(arr){
+    return arr.map(({preview, original, description}) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`).join('')
+};
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionType:'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+
+  });
+  
